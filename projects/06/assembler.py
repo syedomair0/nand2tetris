4bbb2948a3406d3f3b66c5d09863fe1ac552c5c9
Omair Syed <syedomair0@github.com>
2021-07-09 19:09:41 -0500

    add classification of commands

--- projects/06/assembler.py
+++ projects/06/assembler.py
@@ -1,5 +1,10 @@
 #!/opt/homebrew/bin/python3.9
 
-import argparse
+#parser = argparse.ArgumentParser(prog="Hack Assembler", description="Assemble the Hack assembly to binary")
+import sys
 
+with open(sys.argv[1], 'r') as f:
+    for i in f.read().splitlines():
+        if (i and not i.startswith("//")):
+            print(i.strip())
 
