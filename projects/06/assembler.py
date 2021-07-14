6013941123ea483a780186b931a03c2e79d2eb79
Omair Syed <syedomair0@github.com>
2021-07-14 15:34:23 -0500

    progress on assembler

--- projects/06/assembler.py
+++ projects/06/assembler.py
@@ -1,22 +1,11 @@
 #!/usr/bin/python3
 
 import sys
-import re
 from pprint import pprint as pp
 
 """Not efficient at all!!! Write better code"""
 
-jump_table = {"null":"000",
-              "JGT" :"001",
-              "JEQ" :"010",
-              "JGE" :"011",
-              "JLT" :"100",
-              "JNE" :"101",
-              "JLE" :"110",
-              "JMP" :"111"}
-
-
-dest_table = {"null":"000",
+dest_table = {None  :"000",
               "M"   :"001",
               "D"   :"010",
               "MD"  :"011",
@@ -25,7 +14,7 @@ dest_table = {"null":"000",
               "AD"  :"110",
               "AMD" :"111"}
 
-comp_table = {"0": {"0"  : "101010",
+comp_table = {0: {"0"  : "101010",
                       "1"  : "111111",
                       "-1" : "111010",
                       "D"  : "001100",
@@ -43,7 +32,7 @@ comp_table = {"0": {"0"  : "101010",
                       "A-D": "000111",
                       "D&A": "000000",
                       "D|A": "010101"},
-                "1": {"M" : "110000",
+                1: {"M" : "110000",
                       "!M": "110001",
                       "-M": "110011",
                       "M+1": "110111",
@@ -55,16 +44,25 @@ comp_table = {"0": {"0"  : "101010",
                       "D|M": "010101",}
                 }
 
+jump_table = {None  :"000",
+              "JGT" :"001",
+              "JEQ" :"010",
+              "JGE" :"011",
+              "JLT" :"100",
+              "JNE" :"101",
+              "JLE" :"110",
+              "JMP" :"111"}
+
+
 def parser(cmd):
     ret = ""
     dest, comp, jmp = None, None, None
-    if(cmd.startswith('@')):
+    if(cmd.startswith('@') and cmd[1:].isnumeric() and (int(cmd[1:]) < 2**15)):
         """
         there needs to be a condition here for resolving symbols and integers
         """
-        if(cmd[1:].isnumeric() and (int(cmd[1:]) < 2**15)):
-            ret += f'{int(cmd[1:]):016b}'
-            return ret
+        ret += f'{int(cmd[1:]):016b}'
+        return ret
     elif("=" in cmd or ";" in cmd):
         if('=' in cmd):
             dest,comp = cmd.split('=')
@@ -72,18 +70,35 @@ def parser(cmd):
                 comp, jmp = comp.split(';')
         if(';' in cmd):
             comp,jmp = cmd.split(';')
-        return(f'dest={dest}, comp={comp}, jump={jmp}')
+
+    comp_signal = 0 
+    if(comp is not None and ('M' in comp)):
+        comp_signal = 1
+    return(f'dest={dest_table[dest]} comp={comp_table[comp_signal][comp]} jump={jump_table[jmp]}')
 
 def code():
     pass
 
-def build_symbol_table():
-    pass
+def build_symbol_table(commands):
+    symbol_table = {}
+    for line, cmd in commands.items():
+        if(cmd.startswith('(')):
+            label = cmd[1:len(cmd)-1]
+        elif(cmd.startswith('@') and not cmd[1:].isnumeric() and cmd[1:len(cmd)-1] not in symbol_table):
+            label = cmd
+
+        if label not in symbol_table:
+            symbol_table[label] = line
+
+
+    return(symbol_table)
 
 with open(sys.argv[1], 'rt') as f:
-    commands = {counter : [command.strip().partition(' ')[0], True if command.startswith("(") else False, parser(command.strip().partition(' ' )[0])]
+    commands = {counter : command.strip().partition(' ')[0]# parser(command.strip().partition(' ' )[0])]
                     for counter, command in enumerate(f.read().splitlines(), start=1)
-                        if (not command.startswith("//") and command) }
+                        if (command and not command.startswith("//") ) }
+    
 
 if __name__ == "__main__":
     pp(commands)
+    pp(build_symbol_table(commands))
